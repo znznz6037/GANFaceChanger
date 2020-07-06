737eed15d516f53dec8d73692392fe6a9d1c3af7
SeBeom Park <znznz6037@gmail.com>
2020-07-06 18:03:18 +0900

    Updates

---
+++ precache-manifest.3e21b96f4174be383bfb75911bcaa26f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2626424004a854cab0981ebc84b03e",
+    "url": "/GANFaceChanger/index.html"
+  },
+  {
+    "revision": "18047e5e422ad23dc19c",
+    "url": "/GANFaceChanger/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4484a3daae44a2cdc7ee",
+    "url": "/GANFaceChanger/static/js/2.3278881e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GANFaceChanger/static/js/2.3278881e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18047e5e422ad23dc19c",
+    "url": "/GANFaceChanger/static/js/main.14e7549b.chunk.js"
+  },
+  {
+    "revision": "48acf73954a06e843955",
+    "url": "/GANFaceChanger/static/js/runtime-main.05c97524.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GANFaceChanger/static/media/logo.5d5d9eef.svg"
+  }
+]);
