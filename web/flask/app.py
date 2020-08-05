0e8b03a2c765ae5acbe2b9057c52aca45efb508e
znznz6037 <root@psb.asia-northeast3-a.c.rosy-gantry-285407.internal>
2020-08-05 09:26:51 +0000

    Flask API서버

--- web/flask/app.py
+++ web/flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 from flask_restful import Resource, Api
 from flask.views import MethodView
 from werkzeug.utils import secure_filename
@@ -9,11 +9,19 @@ import subprocess
 
 port = 5000
 log = logging.basicConfig(filename='testsvr.log', level=logging.INFO)
+#app = Flask(__name__, static_url_path="", static_folder = "static")
 app = Flask(__name__)
 api = Api(app)
-UPLOAD_DIR="./image"
+UPLOAD_DIR="./data/celeba/images"
+RESULT_DIR="./static/image/results"
 app.config['UPLOAD_DIR'] = UPLOAD_DIR
+app.config['RESULT_DIR'] = RESULT_DIR
 app.config['JSON_AS_ASCII'] = False
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+image = 'static/image/results/1-images.jpg'
+if os.path.isfile(image):
+    os.remove(image)
 
 @app.route('/')
 def Home():
@@ -28,18 +36,12 @@ def upload():
         path = os.path.join(app.config['UPLOAD_DIR'], fname)
         f.save(path)
         print(path)
-        os.chdir('./stargan-master')
-        subprocess.call(["python", "main.py", "--mode", "test", "--dataset", "CelebA", "--image_size", "128", "--c_dim" ,"5", "--model_save_step", "1", "--log_step", "1", "--sample_step", "1", "--lr_update_step", "1", "--selected_attrs","Black_Hair", "Blond_Hair", "Brown_Hair", "Male", "Young", "--model_save_dir=stargan_celeba_128/models", "--result_dir=image/results"])
-        os.chdir('../')
-        return render_template('result.html')
-        #return 'File upload complete (%s)' % path
+        subprocess.call(["python3", "main.py", "--mode", "test", "--dataset", "CelebA", "--image_size", "128", "--c_dim" ,"5", "--model_save_step", "1", "--filename", fname, "--log_step", "1", "--sample_step", "1", "--lr_update_step", "1", "--selected_attrs","Black_Hair", "Blond_Hair", "Brown_Hair", "Male", "Young", "--model_save_dir=stargan_celeba_128/models", "--result_dir=static/image/results"])
+        time.sleep(2)
+        #result = os.path.join(app.config['RESULT_DIR'], secure_filename('1-images.jpg'))
+        image.encode('utf-8')
+        return render_template('result.html', user_image=image)
 
 if __name__=='__main__':
     logging.info('start server')
     app.run('0.0.0.0', port=port, debug=True)
-
-
-	#os.chdir('/stargan-master')
-        #subprocess.call(["python", "main.py", "--mode", "test", "--dataset", "CelebA", "--image_size", "128", "--c_dim" ,"5", "--selected_attrs", "Black_Hair", "Blond_Hair", "Brown_Hair", "Male", "Young", "--model_save_dir='stargan_celeba_128/models'", "--result_dir='stargan_celeba_128/results'"])
-     	#os.chdir('../')
-
